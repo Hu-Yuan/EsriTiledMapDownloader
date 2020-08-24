0dc7e77ee2e6d197828747bf26322523506df71f
Hu-Yuan <martinmoring@foxmail.com>
2020-08-24 15:39:12 +0800

    bug fixed

--- README.md
+++ README.md
@@ -1,5 +1,12 @@
 # EsriTiledMapDownloader
 
-用于下载ArcGIS Server制作的切片地图
+用于下载ArcGIS Server制作的切片地图。
 
-下载完成后可部署至Web服务器进行离线调用，或者配合服务对应的切片方案在ArcMap中进行浏览。
+下载完成后可部署至Web服务器（或GIS服务器）进行离线调用，或者配合服务对应的切片方案在ArcMap中进行浏览。
+
+#Usage:
+python downloadmap.py startlevel endlevel startx starty endx endy
+
+#Example:
+python downloadmap.py 0 4 0 0 0 0
+python downloadmap.py 8 16 54848 102528 55103 102912
--- downloadmap.py
+++ downloadmap.py
@@ -1,31 +1,32 @@
-import os,sys,urllib
+import os,sys,urllib2
 from multiprocessing import Pool,cpu_count
 
-mapUrl = 'http://services.arcgisonline.com/ArcGIS/rest/services/World_Imagery/MapServer/tile'
+mapUrl = 'http://services.arcgisonline.com/ArcGIS/rest/services/World_Imagery/MapServer'
+tileDir = r'D:/coding/imagerytile'
+
 def Download( extent ):
     #print extent
-    try:
-        currentLevel = extent[0]
-        dire = r'D:/3Coding/esriimagery/_alllayers/L{0}/'.format(str(currentLevel).zfill(2))
-        x=extent[1]
-        startY=extent[2]
-        endY=extent[3]
-        Rdire = dire + 'R' + hex(x).split('x')[1].zfill(8)
-        os.makedirs(Rdire)
-        for y in range(startY,endY+1):
+    currentLevel = extent[0]
+    dire = tileDir+'/_alllayers/L{0}/'.format(str(currentLevel).zfill(2))
+    x=extent[1]
+    startY=extent[2]
+    endY=extent[3]
+    Rdire = dire + 'R' + hex(x).split('x')[1].zfill(8)
+    os.makedirs(Rdire)
+    for y in range(startY,endY+1):
+        trytimes=3
+        for i in range(trytimes):
+            Cdire = Rdire + r'/' + 'C' + '{0}.jpg'.format(hex(y).split('x')[1].zfill(8))
             try:
-                response = urllib.urlopen(mapUrl+'/{0}/{1}/{2}'.format(currentLevel,x,y))
+                response = urllib2.urlopen(mapUrl+'/tile'+'/{0}/{1}/{2}'.format(currentLevel,x,y),timeout=3)
                 img=response.read()
-                Cdire = Rdire + r'/' + 'C' + '{0}.png'.format(hex(y).split('x')[1].zfill(8))
                 with open(Cdire,'wb') as f:
                     f.write(img)
+                break
             except Exception as e:
-                print e
-    except:
-        print 'error'
-    else:
-        return
-    #print '{0} done!'.format(x)
+                continue
+            print currentLevel,Rdire,Rdire
+
 
 if __name__ == '__main__':
     if len(sys.argv) == 7:
@@ -42,14 +43,15 @@ if __name__ == '__main__':
             GPpool.map_async(Download, [[currentLevel,x,startY,endY] for x in range(startX,endX+1)])
             GPpool.close()
             GPpool.join()
-            print '{0} level is complete!'.format(currentLevel)
+            print 'level {0} is complete.'.format(currentLevel)
             startX = startX*2
             startY = startY*2
             endX = endX*2+1
             endY = endY*2+1
 			
-#Using:
-#python get_online.py startlevel endlevel startx starty endx endy
+#Usage:
+#python downloadmap.py startlevel endlevel startx starty endx endy
 
 #Example:
-#python get_online.py 8 16 54848 102528 55103 102912
+#python downloadmap.py 0 4 0 0 0 0
+#python downloadmap.py 8 16 54848 102528 55103 102912
